e5f55c234d56f8fcd652102e7a130b6ecab4f80f
baselemadhacker <100936003+baselemadhacker@users.noreply.github.com>
2023-11-27 17:35:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+for (let index = 0; index < array.length; index++) {
+    let number = ["d","c","b","a",["1","2","3","4"]];
+    console.log(number);
+}
+/**number.push("e");**/
+/**number.pop("number[1][3]");**/
+
+
+
